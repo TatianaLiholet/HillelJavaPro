1537a6509b90876d74fd1dd0da096b15ef5425f1
Tetiana Lykholot <tetiana_lykholot@epam.com>
2022-09-26 21:56:50 +0300

    add task 3 lesson 3

---
+++ src/lesson3/HomeWorkApp.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        System.out.println("2 task:");
+        TaskMethods.printThreeWords();
+        System.out.println("3 task:");
+        TaskMethods.checkSumSign(-5, -6);
+        System.out.println("4 task:");
+        TaskMethods.printColor(100);
+        System.out.println("5 task:");
+        TaskMethods.compareNumbers(5, 3);
+        System.out.println("6 task:");
+        TaskMethods.compareSumOfNumbers(10, 10);
+        System.out.println("7 task:");
+        TaskMethods.comparePositiveOrNegative(-6);
+        System.out.println("8 task:");
+        TaskMethods.comparePositiveOrNegativeValue(-9);
+        System.out.println("9 task:");
+        TaskMethods.printText("Hi world", 5);
+        System.out.println("10 task:");
+        TaskMethods.yearLeapYear(64);
+
+    }
+}
